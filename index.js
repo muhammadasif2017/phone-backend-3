fd419a4e749923239d09055c1443d6fa65154de7
Muhammad Asif <muhammadasifhaqnawaz2019@gmail.com>
2024-11-18 00:36:44 +0500

    deploy backend to flyio service

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+app.use(express.json());
+
+const cors = require("cors");
+
+app.use(cors());
+
+morgan.token("req-body", (req) => {
+  return req.body && Object.keys(req.body).length
+    ? JSON.stringify(req.body)
+    : "No Body";
+});
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      tokens["req-body"](req, res),
+    ].join(" ");
+  })
+);
 
 let persons = [
   {
@@ -25,10 +51,10 @@ let persons = [
 ];
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons)
+  response.json(persons);
 });
 
-app.get('/info', (request, response) => {
+app.get("/info", (request, response) => {
   const responseStr = `<div>
     <p>Phonebook has info for ${persons.length} people</p>
     <p>${new Date()}</p>
@@ -36,51 +62,66 @@ app.get('/info', (request, response) => {
   response.send(responseStr);
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
   if (person) {
-    response.json(person)
+    response.json(person);
   } else {
-    response.status(404).end()
+    response.status(404).end();
   }
-})
+});
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-  const person = persons.find(p => p.id === id);
+  const person = persons.find((p) => p.id === id);
 
   console.log(person);
 
   if (!person) {
-    return response.status(404).send('Person not found');
+    return response.status(404).send("Person not found");
   }
 
-  persons = persons.filter(p => p.id !== id);
+  persons = persons.filter((p) => p.id !== id);
   console.log(persons);
   response.status(204).end();
 });
 
-const findIdForPerson = () => Math.floor(Math.random() * 1000000);
-
-app.post('/api/persons', (request, response) => {
+const generateId = () => {
+  const maxId =
+    persons.length > 0 ? Math.max(...persons.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
+app.post("/api/persons", (request, response) => {
   const body = request.body;
-
+  console.log(body);
+  const id = Math.random(0, 1010100101);
   if (!body.name || !body.number) {
-    return response.status(400).json({ error: 'Missing required values' });
+    return response.status(400).json({
+      error: "name or number is missing",
+    });
   }
-
-  const { name, number } = body;
-  const personAlreadyExist = persons.find(person => person.name === name);
-  if (personAlreadyExist) {
-    return response.status(409).json({ error: 'name must be unique' });
+  if (persons.find((person) => person.name === body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
   }
-  const id = findIdForPerson();
-  persons = [...persons, { id, name, number }];
-  response.status(201).json({ id, name, number });
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+  persons = persons.concat(person);
+  response.json(persons);
 });
 
-const PORT = 3001;
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
